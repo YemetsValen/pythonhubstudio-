ae1577de7cde258e087874628f1da3d829521da3
Valentin <ramakot666@gmail.com>
2024-10-27 02:32:28 -0300

    add goods

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request) -> HttpResponse:
+    context = {"title": "Home - Главная", "content": "Магазин мебели HOME"}
+    return render(request, "main/index.html", context)
+
+
+def about(request) -> HttpResponse:
+    context = {
+        "title": "О нас",
+        "content": "всё про нас",
+        "text_a": "hola como estas sobre nosotros",
+    }
+    return render(request, "main/about.html", context)
